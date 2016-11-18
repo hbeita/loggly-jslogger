c2328fbd36bace4ce959b1daa8d5e94efbc8b27e
Hector Beita <hgarcia@cecropiasolutions.com>
2016-11-18 16:29:33 -0600

    Testing commit for removing the restriction of update URL tags only when useDomainProxy

--- src/loggly.tracker.js
+++ src/loggly.tracker.js
@@ -4,46 +4,46 @@
     LOGGLY_SESSION_KEY = 'logglytrackingsession',
     LOGGLY_SESSION_KEY_LENGTH = LOGGLY_SESSION_KEY.length + 1,
     LOGGLY_PROXY_DOMAIN = 'loggly';
-    
+
     function uuid() {
         // lifted from here -> http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
-        });  
+        });
     }
-    
+
     function LogglyTracker() {
         this.key = false;
 	 this.sendConsoleErrors = false;
         this.tag = 'jslogger';
         this.useDomainProxy = false;
     }
-    
+
     function setKey(tracker, key) {
         tracker.key = key;
         tracker.setSession();
         setInputUrl(tracker);
     }
-    
-    function setTag(tracker, tag){		
-        tracker.tag = tag;		
-    }	
-    
+
+    function setTag(tracker, tag){
+        tracker.tag = tag;
+    }
+
     function setDomainProxy(tracker, useDomainProxy){
         tracker.useDomainProxy = useDomainProxy;
         //refresh inputUrl value
         setInputUrl(tracker);
     }
-    
+
     function setSendConsoleError(tracker, sendConsoleErrors) {
 	tracker.sendConsoleErrors = sendConsoleErrors;
-		
+
 	if(tracker.sendConsoleErrors === true){
             var _onerror = window.onerror;
             //send console error messages to Loggly
             window.onerror = function (msg, url, line, col){
-                tracker.push({ 
+                tracker.push({
                     category: 'BrowserJsException',
                     exception: {
                         message: msg,
@@ -52,36 +52,36 @@
                         colno: col,
                     }
                 });
-				
+
                 if (_onerror && typeof _onerror === 'function') {
                     _onerror.apply(window, arguments);
                 }
             };
         }
     }
-    
+
     function setInputUrl(tracker) {
-        
+
         if(tracker.useDomainProxy == true){
-            tracker.inputUrl = LOGGLY_INPUT_PREFIX 
+            tracker.inputUrl = LOGGLY_INPUT_PREFIX
             + window.location.host
             + '/'
             + LOGGLY_PROXY_DOMAIN
             + '/inputs/'
-            + tracker.key 
+            + tracker.key
             + '/tag/'
             + tracker.tag;
         }
         else{
-            tracker.inputUrl = LOGGLY_INPUT_PREFIX 
+            tracker.inputUrl = LOGGLY_INPUT_PREFIX
             + (tracker.logglyCollectorDomain || LOGGLY_COLLECTOR_DOMAIN)
             + '/inputs/'
-            + tracker.key 
+            + tracker.key
             + '/tag/'
             + tracker.tag;
         }
     }
-    
+
     LogglyTracker.prototype = {
         setSession: function(session_id) {
             if(session_id) {
@@ -97,14 +97,14 @@
         },
         push: function(data) {
             var type = typeof data;
-            
+
             if( !data || !(type === 'object' || type === 'string') ) {
                 return;
             }
-            
+
             var self = this;
-            
-            
+
+
             if(type === 'string') {
                 data = {
                     'text': data
@@ -114,49 +114,49 @@
                     self.logglyCollectorDomain = data.logglyCollectorDomain;
                     return;
                 }
-                
+
 		if(data.sendConsoleErrors !== undefined) {
 		    setSendConsoleError(self, data.sendConsoleErrors);
                 }
-               	
+
 		if(data.tag) {
                     setTag(self, data.tag);
                 }
-		
-		if(data.useDomainProxy){
-                    setDomainProxy(self, data.useDomainProxy);
-                }
-				
+
+		// if(data.useDomainProxy){
+        //             setDomainProxy(self, data.useDomainProxy);
+        //         }
+        setDomainProxy(self, data.useDomainProxy); //This needs to happens always actuallyy on those cases where the tags are set dynamically
                 if(data.logglyKey) {
                     setKey(self, data.logglyKey);
                     return;
                 }
-                
+
                 if(data.session_id) {
                     self.setSession(data.session_id);
                     return;
                 }
             }
-            
+
             if(!self.key) {
                 return;
             }
-            
+
             self.track(data);
-            
-            
+
+
         },
         track: function(data) {
             // inject session id
             data.sessionId = this.session_id;
-            
+
             try {
                 //creating an asynchronous XMLHttpRequest
                 var xmlHttp = new XMLHttpRequest();
 		xmlHttp.open('POST', this.inputUrl, true); //true for asynchronous request
 		xmlHttp.setRequestHeader('Content-Type', 'text/plain');
 		xmlHttp.send(JSON.stringify(data));
-                
+
             } catch (ex) {
                 if (window && window.console && typeof window.console.log === 'function') {
                     console.log("Failed to log to loggly because of this exception:\n" + ex);
@@ -182,11 +182,11 @@
             document.cookie = LOGGLY_SESSION_KEY + '=' + value;
         }
     };
-    
+
     var existing = window._LTracker;
-    
+
     var tracker = new LogglyTracker();
-    
+
     if(existing && existing.length ) {
         var i = 0,
         eLength = existing.length;
@@ -194,9 +194,9 @@
             tracker.push(existing[i]);
         }
     }
-    
+
     window._LTracker = tracker; // default global tracker
-    
+
     window.LogglyTracker = LogglyTracker;   // if others want to instantiate more than one tracker
-    
+
 })(window, document);
